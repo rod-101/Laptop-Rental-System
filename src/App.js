f08130baa8709768567f5167f970230a544ff5b1
Rodjones Rosalinda <rodjonesrosalinda@gmail.com>
2024-10-02 06:52:47 +0800

    changed the test to match the string 'Welcome to Lappify!'

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 function Home() {
     return (
         <div>
-            <h1>Welcome to Lappify!!</h1>
+            <h1>Welcome to Lappify!</h1>
             <p>Lending and renting a laptop made easier.</p><br></br>
             
             <p>Choose what you want to do.</p>
