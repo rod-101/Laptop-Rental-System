608f9dde536e273f5c9b43e486f91167427201d9
Rodjones Rosalinda <rodjonesrosalinda@gmail.com>
2024-10-02 14:47:51 +0800

    added babel in dev dependencies

--- src/App.js
+++ src/App.js
@@ -15,9 +15,7 @@ function Home() {
             <p>Lending and renting a laptop made easy.</p><br></br>
             
             <p>Choose what you want to do.</p>
-            <button>Lend</button>
-            <span>or</span>
-            <button>Rent</button>
+            <button>Lend</button><span>or</span><button>Rent</button>
         </div>
     );
 }
