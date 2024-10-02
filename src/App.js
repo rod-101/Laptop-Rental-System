fda7e6ce676f517879177024fba9ddbdb56d1ff2
Rodjones Rosalinda <rodjonesrosalinda@gmail.com>
2024-10-02 08:20:47 +0800

    some paragraph changes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function Home() {
     return (
         <div>
             <h1>Welcome to Lappify!</h1>
-            <p>Lending and renting a laptop made easier.</p><br></br>
+            <p>Lending and renting a laptop made easy.</p><br></br>
             
             <p>Choose what you want to do.</p>
             <button onClick={() => <Rent />}>Lend</button>
