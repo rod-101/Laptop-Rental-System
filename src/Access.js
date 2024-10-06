1ece9e50657ff1f2fd1a1a51bb07255e962b9275
Rodjones Rosalinda <rodjonesrosalinda@gmail.com>
2024-10-06 20:31:05 +0800

    added state userType in Access page heading

--- src/Access.js
+++ src/Access.js
@@ -1,14 +1,15 @@
-export default function Access() {
+export default function Access(props) {
+    console.log('this is a text logged from the access component, the userType is ' + props.userType);
     return (
         <form id="formHeader">
-            <h1>Create a userType Account</h1>
+            <h1>Create a {props.userType} Account</h1>
 
             <div id="formBody">
                 <input type="text" placeholder="Username" />
                 <input type="text" placeholder="Email" />
                 <input type="text" placeholder="Password" />
                 
-                <p>or <a href="https://google.com">Login</a></p>
+                <p>or <span>Login</span></p>
                 <input type="submit" value="Register/Login" />
             </div>
         </form>
--- src/Welcome.js
+++ src/Welcome.js
@@ -1,14 +1,31 @@
+import React, {useState} from 'react';
+import Access from './Access.js'
 
 export default function Welcome() {
-   
+    const [userType, setUser] = useState(null);
+    
+    const buttonLender = user => {
+        setUser('Lender')
+    }
+    const buttonRenter = user => setUser('Renter')
+
     return (
         <div>
-            <h1>Welcome to Lappify!</h1>
-            <p>Lending and renting a laptop made easy.</p><br></br>
-            
-            <p>Choose what you want to do.</p>
-            <button value="lender">Lend</button> <span>or </span>  
-            <button value="renter">Rent</button>
+            {
+                userType ? (<Access userType={userType} />) : (
+                    <div>
+                        <h1>Welcome to Lappify!</h1>
+                        <p>Lending and renting a laptop made easy.</p><br></br>
+                        
+                        <p>Choose what you want to do.</p>
+                        <button value="lender" onClick={buttonLender}>Lend</button> <span>or </span>  
+                        <button value="renter" onClick={buttonRenter}>Rent</button>
+                    </div>
+                )
+            }
         </div>
     );
+   
+    
+    
 }
