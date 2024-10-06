982bc9a67e89c67954d17849faf3f73a571703d9
Rodjones Rosalinda <rodjonesrosalinda@gmail.com>
2024-10-06 20:58:03 +0800

    create logic for display of login and signup

--- src/Access.js
+++ src/Access.js
@@ -1,18 +1,47 @@
-export default function Access(props) {
-    console.log('this is a text logged from the access component, the userType is ' + props.userType);
-    return (
-        <form id="formHeader">
-            <h1>Create a {props.userType} Account</h1>
+import React, { useState } from 'react';
 
-            <div id="formBody">
-                <input type="text" placeholder="Username" />
-                <input type="text" placeholder="Email" />
-                <input type="text" placeholder="Password" />
-                
-                <p>or <span>Login</span></p>
-                <input type="submit" value="Register/Login" />
-            </div>
-        </form>
-        
-    );
+export default function Access(props) {
+    const [state, setState] = useState('Login');
+    const userType = props.userType;
+    
+    const toggleAccess = () => {
+        if(state === 'Login') {
+            setState('Signup')
+        } else {
+            setState('Login')
+        }
+    }
+
+    if(state === 'Login') {
+        return (
+            <form id="formHeader">
+                <h1>Log in to a {userType} Account</h1>
+    
+                <div id="formBody">
+                    <input type="text" placeholder="Email" />
+                    <input type="text" placeholder="Password" />
+                    
+                    <p>or <span onClick={toggleAccess}>Sign up</span></p>
+                    <input type="submit" value={state} />
+                </div>
+            </form>
+        );
+    } else if(state === 'Signup') {
+        return (
+            <form id="formHeader">
+                <h1>Create a {userType} Account</h1>
+    
+                <div id="formBody">
+                    <input type="text" placeholder="Username" />
+                    <input type="text" placeholder="Email" />
+                    <input type="text" placeholder="Password" />
+                    
+                    <p>or <span onClick={toggleAccess}>Login</span></p>
+                    <input type="submit" value={state} />
+                </div>
+            </form>
+        );
+    }
+
+    
 }
