f235b0380780d55516def35e0c723028413bc755
Rodjones Rosalinda <rodjonesrosalinda@gmail.com>
2024-10-09 16:00:48 +0800

    change app.test.js text matcher

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Welcome to Lappify!/i);
+  const linkElement = screen.getByText(/Welcome to/i);
   expect(linkElement).toBeInTheDocument();
 });
