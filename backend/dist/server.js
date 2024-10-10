a063247334e958083d5dcc9ec7ce6a62d656d982
Rodjones Rosalinda <rodjonesrosalinda@gmail.com>
2024-10-10 13:09:23 +0800

    transpiled both server and database js files

---
+++ backend/dist/server.js
@@ -0,0 +1,50 @@
+"use strict";
+
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001;
+const cors = require('cors');
+const dbclient = require('./database.js');
+app.use(express.json());
+app.use(cors());
+app.post('/login', (req, res) => {
+  const {
+    email,
+    password
+  } = req.body;
+  const queryString = `SELECT * FROM users WHERE email = $1 AND password = $2`;
+  dbclient.query(queryString, [email, password], (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Error querying the database');
+    }
+    if (result.rows.length > 0) {
+      console.log("Got the data", result.rows[0]);
+      return res.json(result.rows[0]); // Return the matched user data as JSON
+    } else {
+      return res.status(401).send('Invalid email or password');
+    }
+  });
+});
+app.get('/data/:id', (req, res) => {
+  const {
+    id
+  } = req.params;
+  dbclient.query(`SELECT * FROM users WHERE user_id = ${id}`, (err, result) => {
+    if (err) throw err;
+    res.json(result);
+  });
+});
+app.get('/data', (req, res) => {
+  dbclient.query("SELECT * FROM users", (err, result) => {
+    if (err) throw err;
+    res.json(result);
+  });
+});
+app.get('/dbuser', (req, res) => {
+  res.status(200).send(`This route listens for get requests at /dbuser`);
+});
+app.listen(port, () => {
+  console.log(`The server is listening to port ${port}.`);
+});
