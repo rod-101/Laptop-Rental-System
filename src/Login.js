012cf157f3ae9f3146cf5f706779918eb409e31d
Rodjones <102003369+rod-101@users.noreply.github.com>
2024-10-10 09:38:02 +0800

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -21,7 +21,7 @@ export default function Login(props) {
         e.preventDefault();
 
         try {
-            const response = await fetch('SERVER_URL/login', {
+            const response = await fetch('/login', {
                 method: "POST",
                 headers: {
                     'Content-Type': 'application/json'
