127b6052f40b84d6e499666b5bb87f208531c8bd
Rodjones <102003369+rod-101@users.noreply.github.com>
2024-10-17 11:54:04 +0800

    Update ProfileComponent.js

--- src/ProfileComponent.js
+++ src/ProfileComponent.js
@@ -1,7 +1,7 @@
 import React, { useState, useContext, useEffect, useCallback} from "react"
 import { UserContext } from "./UserContext"
 import { useNavigate } from 'react-router-dom'
-const SERVER_URL = 'http://localhost:3001' || process.env.REACT_APP_SERVER_URL
+const SERVER_URL = process.env.REACT_APP_SERVER_URLm || 'http://localhost:3001' 
 
 export default function ProfileComponent() {
     const { userData } = useContext(UserContext)
